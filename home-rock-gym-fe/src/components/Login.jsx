8a57789043b31fb3f17a34b4c46f3aa124a293b7
nathan-saygers <nathan.saygers@gmail.com>
2020-06-29 10:16:58 -0700

    remove reactstrap import form hook

--- home-rock-gym-fe/src/components/Login.jsx
+++ home-rock-gym-fe/src/components/Login.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 function Login() {
   return (
-    <div>
-
-    </div>
+    <Form>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+      </FormGroup>
+    </Form>
   )
 }
 
