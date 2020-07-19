171d6efca8d0ff47ade7029f5210ae38ff0fd3a8
nathan-saygers <nathan.saygers@gmail.com>
2020-07-18 21:24:39 -0700

    import wall grid component for styling

--- home-rock-gym-fe/src/App.js
+++ home-rock-gym-fe/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Login from './components/Login';
-import 'normalize.css';
-import './App.css';
+import React from "react";
+import "normalize.css";
+import "./App.css";
+
+// Components
+import Login from "./components/Login";
+import WallGrid from "./components/WallGrid";
 
 function App() {
   return (
     <div>
+      <WallGrid />
       <Login />
     </div>
   );
