a5e159a5698cf67f2f1e310a7fa599eee665a285
nathan-saygers <nathan.saygers@gmail.com>
2020-07-24 19:59:16 -0700

    remove hardcoded data and add func to calculate coords

--- home-rock-gym-fe/src/components/WallGrid.jsx
+++ home-rock-gym-fe/src/components/WallGrid.jsx
@@ -5,45 +5,34 @@ import styles from "./WallGrid.module.scss";
 // components
 import WallGridItem from "./WallGridItem";
 
-let gridShape = [
-  { name: "1A" },
-  { name: "1B" },
-  { name: "1C" },
-  { name: "1D" },
-  { name: "1E" },
-
-  { name: "2A" },
-  { name: "2B" },
-  { name: "2C" },
-  { name: "2D" },
-  { name: "2E" },
-
-  { name: "3A" },
-  { name: "3B" },
-  { name: "3C" },
-  { name: "3D" },
-  { name: "3E" },
-
-  { name: "4A" },
-  { name: "4B" },
-  { name: "4C" },
-  { name: "4D" },
-  { name: "4E" },
-
-  { name: "5A" },
-];
-
 const WallGrid = ({ wallDimensions }) => {
+  const wallX = wallDimensions[0];
+  const wallY = wallDimensions[1];
+
+  const generateCoords = () => {
+    let coordArr = [];
+
+    for (let i = 0; i < wallX * wallY; i++) {
+      const y = parseInt(i / wallX) + 1;
+      const xInt = (i % wallX) + 1;
+      const x = String.fromCharCode(64 + xInt);
+
+      coordArr.push({ name: `${x}${y}` });
+    }
+
+    return coordArr;
+  };
+
   const calculateWallDimensions = () => {
-    const width = wallDimensions[0] * 200 + (wallDimensions[0] - 1) * 16 + 48;
-    const height = wallDimensions[1] * 200 + (wallDimensions[1] - 1) * 16 + 48;
+    const width = wallX * 100 + (wallX - 1) * 16 + 48;
+    const height = wallY * 100 + (wallY - 1) * 16 + 48;
 
     return { width: `${width}px`, height: `${height}px` };
   };
 
   return (
     <div style={calculateWallDimensions()} className={styles.wall}>
-      {gridShape.map((coord) => (
+      {generateCoords().map((coord) => (
         <WallGridItem coord={coord} />
       ))}
     </div>
