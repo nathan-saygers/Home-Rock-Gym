d40937f90bb459a0c3cd683530ee86c57c015eb3
nathan-saygers <nathan.saygers@gmail.com>
2020-09-27 14:37:00 -0700

    Restructure files

--- home-rock-gym-fe/src/components/WallGridItem.jsx
+++
@@ -1,9 +0,0 @@
-// Dependencies
-import React from "react";
-import styles from "./WallGridItem.module.scss";
-
-const WallGridItem = ({ coord }) => {
-  return <div className={styles.gridItem}>{coord.name}</div>;
-};
-
-export default WallGridItem;
