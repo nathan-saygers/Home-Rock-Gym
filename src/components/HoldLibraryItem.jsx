f443d59c3814e0aaaf4a7ecf12f760ee777da39d
nathan-saygers <nathan.saygers@gmail.com>
2020-09-29 14:05:24 -0700

    Create HoldLibraryItem boiler plate

---
+++ src/components/HoldLibraryItem.jsx
@@ -0,0 +1,13 @@
+// Dependencies
+import React from "react";
+import styles from "./HoldLibrary.module.scss";
+
+const HoldLibraryItem = (props) => {
+  return (
+    <div className={styles.HoldLibraryItemContainer}>
+      I'm a hold library item
+    </div>
+  );
+};
+
+export default HoldLibraryItem;
