ccb0e31e54dd8ff909492d00a2174473d1105521
nathan-saygers <nathan.saygers@gmail.com>
2020-09-29 13:35:29 -0700

    create hold library files and boiler plate

---
+++ src/components/HoldLibrary.jsx
@@ -0,0 +1,9 @@
+// Dependencies
+import React from "react";
+import styles from "./HoldLibrary.module.scss";
+
+const HoldLibrary = (props) => {
+  return <div className={styles.holdLibraryContainer}>hold HoldLibrary</div>;
+};
+
+export default HoldLibrary;
