2fb52bdadae7e8ad7258db820499644dd6cc8232
nathan-saygers <nathan.saygers@gmail.com>
2020-09-30 13:28:56 -0700

    Add hold searchbar files and boiler plate

---
+++ src/components/HoldSearchBar.jsx
@@ -0,0 +1,9 @@
+// Dependencies
+import React from "react";
+import styles from "./HoldSearchBar.module.scss";
+
+const HoldSearchBar = (props) => {
+  return <div className={styles.searchBarContainer}>I'm a search bar!</div>;
+};
+
+export default HoldSearchBar;
