8e6f39e7e3c0adbc416ae473f249af5d361e735a
nathan-saygers <nathan.saygers@gmail.com>
2020-10-05 16:12:37 -0700

    Adds addHoldModal files and boiler plate

---
+++ src/components/WallGrid/AddHoldModal.jsx
@@ -0,0 +1,9 @@
+// Dependencies
+import React from "react";
+import styles from "./AddHoldModal.module.scss";
+
+const AddHoldModal = (props) => {
+  return <div className={styles.addHoldModalContainer}>Add hold modal</div>;
+};
+
+export default AddHoldModal;
