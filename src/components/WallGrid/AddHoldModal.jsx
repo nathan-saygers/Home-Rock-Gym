4ade910d22f0514c5aba71820708793f968327ac
nathan-saygers <nathan.saygers@gmail.com>
2020-10-07 13:14:32 -0700

    Add hold options to add hold modal

--- src/components/WallGrid/AddHoldModal.jsx
+++ src/components/WallGrid/AddHoldModal.jsx
@@ -1,9 +1,29 @@
 // Dependencies
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AddHoldModal.module.scss";
 
-const AddHoldModal = (props) => {
-  return <div className={styles.addHoldModalContainer}>Add hold modal</div>;
+const AddHoldModal = ({ holds }) => {
+  const [holdOptions, setHoldOptions] = useState("");
+
+  useEffect(() => {
+    setHoldOptions(holds);
+  }, []);
+
+  return (
+    <div className={styles.addHoldModalContainer}>
+      <form action="">
+        <select name="hold">
+          <option value="">add a hold</option>
+          {holdOptions &&
+            holdOptions.map((hold, index) => (
+              <option value={hold.name} key={index}>
+                {hold.name}
+              </option>
+            ))}
+        </select>
+      </form>
+    </div>
+  );
 };
 
 export default AddHoldModal;
