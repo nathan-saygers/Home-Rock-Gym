52ad49435326cd2c913dcecd644daf605489d0aa
nathan-saygers <nathan.saygers@gmail.com>
2020-10-09 15:43:46 -0700

    imports problemSearchBar component

--- src/components/ProblemLibrary/ProblemLibrary.jsx
+++ src/components/ProblemLibrary/ProblemLibrary.jsx
@@ -4,19 +4,18 @@ import styles from "./ProblemLibrary.module.scss";
 
 // Components
 import ProblemLibraryItem from "./ProblemLibraryItem";
-// import ProblemSearchBar from "./ProblemSearchBar";
+import ProblemSearchBar from "./ProblemSearchBar";
 
-const ProblemLibrary = ({ problems, setProblemData }) => {
+const ProblemLibrary = ({ problems }) => {
   const [filteredProblems, setFilteredProblems] = useState();
   console.log("problems in ProblemLibary", problems);
 
   return (
     <div className={styles.problemLibraryContainer}>
-      {/* <ProblemSearchBar
-        setFilteredproblems={setFilteredproblems}
-        filteredproblems={filteredproblems}
+      <ProblemSearchBar
+        setFilteredProblems={setFilteredProblems}
         problemData={problems}
-      /> */}
+      />
       <div className={styles.problemContainer}>
         <div className={styles.problemTableHeader}>
           <p>Name</p>
@@ -24,8 +23,8 @@ const ProblemLibrary = ({ problems, setProblemData }) => {
           <p>Grade</p>
           <p>Created at</p>
         </div>
-        {problems &&
-          problems.map((problem, index) => (
+        {filteredProblems &&
+          filteredProblems.map((problem, index) => (
             <ProblemLibraryItem problemDetails={problem} key={index} />
           ))}
       </div>
