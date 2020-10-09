17df030ee9a6066ece00f29c011ca236572cdd00
nathan-saygers <nathan.saygers@gmail.com>
2020-10-09 14:48:07 -0700

    import problem library into app

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import Login from "./components/Login";
 import WallGrid from "./components/WallGrid/WallGrid";
 import WallGridInput from "./components/WallGrid/WallGridInput";
 import HoldLibrary from "./components/HoldLibrary/HoldLibrary";
+import ProblemLibrary from "./components/ProblemLibrary/ProblemLibrary";
 
 // Dummy data
 import holdDummyData from "./dummyData/holds";
+import problemDummyData from "./dummyData/problems";
 
 function App() {
   const [wallDimensions, setWallDimensions] = useState([8, 8]);
   const [wallDisplayName, setWallDisplayName] = useState("Wall Name");
   const [holds, setHolds] = useState(holdDummyData);
+  const [problems, setProblems] = useState(problemDummyData);
 
   return (
     <div>
       <div className="pageContainer">
         <HoldLibrary holds={holds} setHoldData={setHolds} />
+        <ProblemLibrary problems={problems} setProblemData={setProblems} />
         <WallGridInput
           setWallDimensions={setWallDimensions}
           setWallDisplayName={setWallDisplayName}
--- src/components/ProblemLibrary/ProblemLibrary.jsx
+++ src/components/ProblemLibrary/ProblemLibrary.jsx
@@ -4,27 +4,30 @@ import styles from "./ProblemLibrary.module.scss";
 
 // Components
 import ProblemLibraryItem from "./ProblemLibraryItem";
-import ProblemSearchBar from "./ProblemSearchBar";
+// import ProblemSearchBar from "./ProblemSearchBar";
 
-const ProblemLibrary = ({ problems }) => {
-  const [filteredproblems, setFilteredproblems] = useState();
-  console.log("filteredproblems", filteredproblems);
+const ProblemLibrary = ({ problems, setProblemData }) => {
+  const [filteredProblems, setFilteredProblems] = useState();
+
   return (
     <div className={styles.problemLibraryContainer}>
-      <problemSearchBar
+      {/* <ProblemSearchBar
         setFilteredproblems={setFilteredproblems}
         filteredproblems={filteredproblems}
         problemData={problems}
-      />
+      /> */}
       <div className={styles.problemContainer}>
         <div className={styles.problemTableHeader}>
           <p>Name</p>
           <p>Size</p>
           <p>Type</p>
           <p>Color</p>
+          <p>another</p>
+          <p>thing</p>
+          <p>problemTableHeader</p>
         </div>
-        {filteredproblems &&
-          filteredproblems.map((problem, index) => (
+        {filteredProblems &&
+          filteredProblems.map((problem, index) => (
             <ProblemLibraryItem problemDetails={problem} key={index} />
           ))}
       </div>
